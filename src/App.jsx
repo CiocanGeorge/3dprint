c87e309d9f7c48eb8ea4c270a778110acd04e474
George <george@videobuzz.io>
2026-03-13 15:42:03 +0200

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/AuthContext";
 import { HomePage } from "./pages/HomePage";
 import { CustomizerPage } from "./pages/CustomizerPage";
@@ -10,6 +10,7 @@ import { ForgotPasswordPage } from "./pages/auth/ForgotPasswordPage";
 import { DashboardPage } from './pages/DashboardPage';
 import LicensePlatePage from "./pages/LicensePlatePage";
 import { LandingPage } from "./pages/LandingPage";
+import { CalculatorPage } from "./pages/dashboard/CalculatorPage";
 
 function PrivateRoute({ children }) {
   const { isAuthenticated, loading } = useAuth();
@@ -35,7 +36,7 @@ function Routes_() {
           </PrivateRoute>
         }
       />
-      <Route
+        <Route
         path="/customizer/license_plate"
         element={
           <PrivateRoute>
@@ -90,9 +91,7 @@ function Routes_() {
 export default function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes_ />
-      </BrowserRouter>
+      <Routes_ />
     </AuthProvider>
   );
 }
